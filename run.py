d0329232b033b52637453bdbb0dbe15148d5d831
greggie87 <>
2023-12-25 17:18:34 +0000

    Add placeholders for hit, miss and complete on game board

--- run.py
+++ run.py
@@ -5,7 +5,23 @@ print("It is you against the computer! Guess where to shoot in order to sink the
 #Rules
 
 #Game
-print("     0  1  2  3  4  5  6  7  8  9")
-print("")
+print("     0  1  2  3  4  5  6  7  8  9\n")
+
+place = 0
 for x in range(10):
-    print(x, " ", " - "*10)
+    row = ""
+    for y in range(10):
+        ch = " - "
+        if place in miss:
+            ch = " x "
+        elif place in hit:
+            ch = " O "
+        elif place in comp:
+            ch = " @ "
+        row = row + ch
+        place = place + 1
+    print(x, " ", row)
+
+hit = [22, 23, 24]
+miss = [11,12,13,14]
+comp = [25]
