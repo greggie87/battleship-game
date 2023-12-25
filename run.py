8d648510656c7de3b7cd2872f972c4d68ac9bf73
greggie87 <>
2023-12-25 16:52:53 +0000

    Create game board layout

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 #Title
-
+print("\nBATTLESHIP GAME!\n")
 #Description
-
+print("It is you against the computer! Guess where to shoot in order to sink the computers ship. First to sink all of the opponents ships is the winner!\n")
 #Rules
 
 #Game
+print("     0  1  2  3  4  5  6  7  8  9")
+print("")
+for x in range(10):
+    print(x, " ", " - "*10)
